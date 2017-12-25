087009c7b962ab36baee9a3a72511f71112737a1
dfackler <dfackler@path2response.com>
2017-12-25 15:06:21 -0700

    removed forms

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort, session
 
 # create instance of Flask class 
 # __name__ is special python variable related to name of app
 app = Flask(__name__)
-
-# create request handler to display form
-@app.route('/form')
-def form():
-    return render_template('form.html')
-
-# create request handler to handle information from form
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-    return render_template(
-        'submitted_form.html',
-        name=name,
-        email=email,
-        site=site,
-        comments=comments)
 
 @app.route('/')
 def simple_add():
@@ -30,6 +11,9 @@ def simple_add():
 @app.route('/result', methods=['POST'])
 def add_result():
     sum = int(request.form['num1']) + int(request.form['num2'])
+    if(sum > 9223372036854775807 or
+        sum < -9223372036854775808-1 ):
+        abort(400)
     return render_template(
         'add_result.html',
         sum=sum)
